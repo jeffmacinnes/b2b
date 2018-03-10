6390f3b4c15fa0cadf45b52de46735c431008045
Jeff MacInnes <jeff.macinnes@gmail.com>
2018-03-09 22:19:10 -0800

    trigger test at scanner code

--- sendTriggerToServer.py
+++ sendTriggerToServer.py
@@ -12,8 +12,10 @@ import sys
 import select
 
 
-host = '127.0.0.1'
-host = '192.168.0.8'
+#host = '127.0.0.1'
+#host = '192.168.0.4'    # IP of the server computer
+#host = '173.250.196.123' # lab iMac
+host = '173.250.153.18' # scanner old laptop
 port = 6666
 
 # create an instance of the socket
--- testServer.py
+++ testServer.py
@@ -12,25 +12,26 @@ import serial
 import time
 
 context = zmq.Context()
-host = '192.168.0.8' # ip address of server computer
+host = '192.168.0.4' # ip address of server computer
+host = '173.250.196.123'  # ip of office comp
 port = 6666
 
 # define the socket using the context
 serverSocket = context.socket(zmq.REP)
-serverSocket.bind("tcp://127.0.0.1:{}".format(port))
+serverSocket.bind("tcp://{}:{}".format(host, port))
 
 # set up serial port parameters
-ser = serial.Serial('/dev/tty.usbmodem14141', 9600)
+#ser = serial.Serial('/dev/tty.usbmodem14141', 9600)
 
 while True:
     message = serverSocket.recv()
     print('server got message: {}'.format(message))
 
     # write to the serial port
-    ser.write('1'.encode('utf-8'));
+    #ser.write('1'.encode('utf-8'));
 
     # read response from serial port
-    print('resp from arduino: ' + str(ser.readline()))
+    #print('resp from arduino: ' + str(ser.readline()))
 
     # send reply to client
     serverSocket.send_string('got it')
